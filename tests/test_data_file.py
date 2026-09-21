5836c9c179ead9ed6ed84d92f7ae53d936c68154
valmir-neto <valmir.neto@icen.ufpa.br>
2026-09-21 16:59:09 -0300

    Refactor comments in test_data_file.py
    Updated comments in the TestDataFile class for clarity and understanding.

--- tests/test_data_file.py
+++ tests/test_data_file.py
@@ -1,28 +1,28 @@
 import os
-import tempfile
+import tempfile 
 import unittest
 
 from storage.data_file import DataFile
 from storage.page import PAGE_SIZE, Page
 
 
-class TestDataFile(unittest.TestCase):
-    def test_page_persistence(self):
-        with tempfile.TemporaryDirectory() as directory:
-            filename = os.path.join(directory, "dados.db")
-            arquivo = DataFile(filename)
+class TestDataFile(unittest.TestCase):  #classe reponsavél por testar o DataFile
+    def test_page_persistence(self): #verifica se a pagina continua persistente
+        with tempfile.TemporaryDirectory() as directory: #cria uma pasta temporária
+            filename = os.path.join(directory, "dados.db") #serve para montar o caminho do arquivo corretamente.
+            arquivo = DataFile(filename) 
             pagina = Page(2)
-            pagina.write(b"MiniDB")
+            pagina.write(b"MiniDB") #esta sendo representando como bytes "b"
 
-            arquivo.write_page(pagina.page_id, pagina.read())
-            pagina_lida = arquivo.read_page(2)
+            arquivo.write_page(pagina.page_id, pagina.read()) #pega os dados da pag e grava no arquivo dados.db 
+            pagina_lida = arquivo.read_page(2) #testa se aquilo que foi gravado pode ser recuperado.
 
-            self.assertIsNotNone(pagina_lida)
-            self.assertEqual(pagina_lida.page_id, 2)
-            self.assertEqual(pagina_lida.read()[:6], b"MiniDB")
-            self.assertEqual(len(pagina_lida.read()), PAGE_SIZE)
+            self.assertIsNotNone(pagina_lida) #verifica se a pag existe
+            self.assertEqual(pagina_lida.page_id, 2) #confere o id da pag
+            self.assertEqual(pagina_lida.read()[:6], b"MiniDB") #verifica se os 6 bytes estão iguais ao miniDB 
+            self.assertEqual(len(pagina_lida.read()), PAGE_SIZE) #verifica se o tamanho da pag = 4096
 
-    def test_page_count(self):
+    def test_page_count(self): #tesa quantas paginas existem
         with tempfile.TemporaryDirectory() as directory:
             filename = os.path.join(directory, "dados.db")
             arquivo = DataFile(filename)
